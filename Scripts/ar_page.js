b3b4b519671a1f3dc002981b4878f5b454442b7d
Glitch (smartify-ar-demo) <none>
2023-10-05 11:41:54 +0000

    🔅▪️ Updated with Glitch

--- Scripts/ar_page.js
+++ Scripts/ar_page.js
@@ -4,49 +4,25 @@ document.addEventListener('DOMContentLoaded', function () {
         // Get references to the necessary DOM elements
         const target = document.getElementById("target");
         const video = document.getElementById("video");
-        // const audio = document.getElementById("backgroundAudio");
         const plane = document.getElementById("videooverlay");
         const startText = document.getElementById("startText");
-        const audioPrompt = document.getElementById("audioPrompt");
         const backgroundImage = document.getElementById("background");
         const backButton = document.getElementById("backButton");
+        const secondTarget = document.getElementById("secondTarget");
+        const dummyObject2 = document.getElementById("dummyObject2");
+        const mainGateModel = document.getElementById("mainGateModel");
 
         // Initialize variables
         var played = false;
-        var userInteracted = false;
+        var modelEntity;
 
-        // Function to check if the device is iOS
-        function isIOS() {
-            return [
-                'iPad Simulator',
-                'iPhone Simulator',
-                'iPod Simulator',
-                'iPad',
-                'iPhone',
-                'iPod'
-            ].includes(navigator.platform)
-            || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-        }
-
-        // Event listener for audio prompt button click
-        // audioPrompt.addEventListener("click", () => {
-        //     audio.play();
-        //     audioPrompt.style.display = "none";
-        //     userInteracted = true;
-        // });
-
-        // Event listener for target found event
+        // Event listener for the first target found event
         target.addEventListener("targetFound", () => {
-            console.log("target found");
+            console.log("Video target found");
             this.found = true;
             if (!played) {
                 startText.style.display = "none";
                 backgroundImage.style.display = "none";
-                // if (audio.paused && isIOS() && !userInteracted) {
-                //     audioPrompt.style.display = "block";
-                // } else {
-                //     audio.play();
-                // }
                 plane.emit("fadein");
                 video.play();
                 video.addEventListener("ended", function videoend(e) {
@@ -54,21 +30,41 @@ document.addEventListener('DOMContentLoaded', function () {
                 }, false);
                 plane.object3D.position.copy(plane.object3D.position);
             }
-            // } else if (audio.paused) {
-            //     audio.play();
-            // }
         });
 
-        // Event listener for target lost event
+        // Event listener for the first target lost event
         target.addEventListener("targetLost", () => {
-            console.log("target lost");
+            console.log("Video target lost");
             this.found = false;
             if (!played) {
                 video.pause();
-                // audio.pause();
                 startText.style.display = "block";
                 backgroundImage.style.display = "block";
             }
+        });
+
+        // Event listener for the second target found event
+        secondTarget.addEventListener("targetFound", () => {
+            console.log("3D Model target found");
+            if (!modelEntity) {
+                let dummyWorldPos2 = new AFRAME.THREE.Vector3();
+                dummyObject2.object3D.getWorldPosition(dummyWorldPos2);
+
+                modelEntity = document.createElement("a-gltf-model");
+                modelEntity.setAttribute('src', mainGateModel.getAttribute('src'));
+                modelEntity.setAttribute("scale", "0.005 0.005 0.005");
+
+                modelEntity.object3D.position.copy(dummyWorldPos2);
+                modelEntity.object3D.quaternion.copy(dummyObject2.object3D.getWorldQuaternion(new AFRAME.THREE.Quaternion()));
+
+                this.el.sceneEl.appendChild(modelEntity);
+            }
+        });
+
+        // Event listener for the second target lost event
+        secondTarget.addEventListener("targetLost", () => {
+            console.log("3D Model target lost");
+            // Logic for unloading the 3D model can be added here if needed
         });
 
         // Event listener for arframe event
