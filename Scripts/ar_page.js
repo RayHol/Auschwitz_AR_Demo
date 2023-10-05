69340f2457778216fd8743f2cce2bd9fe35230b9
Glitch (smartify-ar-demo) <none>
2023-10-05 13:27:05 +0000

    🗝️#️⃣ Updated with Glitch

--- Scripts/ar_page.js
+++ Scripts/ar_page.js
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
             const target = document.getElementById("target");
             const secondTarget = document.getElementById("secondTarget");
             const video = document.getElementById("video");
+            const audioButton = document.getElementById("audioButton");
+            const audioPromptIcon = document.getElementById("audioPromptIcon")
             const plane = document.getElementById("videooverlay");
             const startText = document.getElementById("startText");
             const backgroundImage = document.getElementById("background");
@@ -26,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log("Video target found");
                 this.found = true;
                 hideTextAndBackground();
+                document.getElementById("audioPrompt").style.display = "flex"; 
                 if (!played) {
                     plane.emit("fadein");
                     video.play();
@@ -40,6 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log("Video target lost");
                 this.found = false;
                 hideTextAndBackground();
+                document.getElementById("audioPrompt").style.display = "none";
                 if (!played) {
                     video.pause();
                     startText.style.display = "block";
@@ -74,39 +78,48 @@ document.addEventListener('DOMContentLoaded', function () {
             }, 3000);
         }
     });
+        function toggleAudio() {
+          const video = document.getElementById("video");  // Get a reference to the video element
+          video.muted = !video.muted;
+
+          const audioPromptIcon = document.getElementById("audioPromptIcon");  // Icon update
+          if (video.muted) {
+              audioPromptIcon.src = '.\\Assets\\mute-icon.svg';
+          } else {
+              audioPromptIcon.src = '.\\Assets\\unmute-icon.svg';  // Replace with your unmute icon path
+          }
+      }
+
+    // Attach the toggle function to button click
+    const audioButton = document.getElementById("audioButton");  // Get a reference to the audio button
+    audioButton.addEventListener('click', toggleAudio);
 
     // Wait for the A-Frame scene to be fully loaded
     document.querySelector('a-scene').addEventListener('loaded', function () {
         // Code for touch controls
-        let modelContainer = document.getElementById("secondTarget"); // assuming the model is within this entity
-
-        let initialScale = { x: 0.004, y: 0.004, z: 0.004 };
-        let initialRotation = { x: 0, y: 0, z: 0 };
-
-        let scale = { ...initialScale };
-        let rotation = { ...initialRotation };
-
+        let modelContainer = document.getElementById("secondTarget");  // assuming the model is within this entity
         modelContainer.addEventListener("touchmove", function(event) {
             if (event.touches.length === 1) {
+                let rotation = modelContainer.getAttribute("rotation");
                 let movementX = event.touches[0].clientX - event.touches[0].radiusX;
                 let movementY = event.touches[0].clientY - event.touches[0].radiusY;
 
                 rotation.x += movementY * 0.1;
                 rotation.y += movementX * 0.1;
 
-                modelContainer.setAttribute("rotation", `${rotation.x} ${rotation.y} ${rotation.z}`);
+                modelContainer.setAttribute("rotation", rotation);
             }
-
             if (event.touches.length === 2) {
+                let scale = modelContainer.getAttribute("scale");
                 let dx = event.touches[0].clientX - event.touches[1].clientX;
                 let dy = event.touches[0].clientY - event.touches[1].clientY;
                 let distance = Math.sqrt(dx * dx + dy * dy);
 
-                scale.x = initialScale.x * distance * 0.0001;
-                scale.y = initialScale.y * distance * 0.0001;
-                scale.z = initialScale.z * distance * 0.0001;
+                scale.x = distance * 0.0001;
+                scale.y = distance * 0.0001;
+                scale.z = distance * 0.0001;
 
-                modelContainer.setAttribute("scale", `${scale.x} ${scale.y} ${scale.z}`);
+                modelContainer.setAttribute("scale", scale);
             }
         });
     });
